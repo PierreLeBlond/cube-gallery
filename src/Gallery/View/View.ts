61208885490e23d7114f873524bc68d8b0a46f77
PierreLeBlond <pierre.lespingal@gmail.com>
2022-03-02 16:30:01 +0100

    style: Change navigation colors

--- src/Gallery/View/View.ts
+++ src/Gallery/View/View.ts
@@ -1,20 +1,24 @@
-import { EventEmitter } from "events";
-import Model from "../Model/Model";
-import dots from "./dots.gif";
-import { css, keyframes } from "@emotion/css";
+import {css, keyframes} from '@emotion/css';
+import {EventEmitter} from 'events';
 
-const mod = (z: number, n: number) => {
-  const m = (( z % n) + n) % n;
-  return m < 0 ? m + Math.abs(n) : m;
-}
+import Model from '../Model/Model';
+
+import dots from './dots.gif';
+
+const mod =
+    (z: number, n: number) => {
+      const m = ((z % n) + n) % n;
+      return m < 0 ? m + Math.abs(n) : m;
+    }
 
 const getNextIndex = (current: number, direction: number, length: number) => {
   return mod(current + direction, length);
 };
 
-const getNextElement = (array: any[], current: number, direction: number) => {
-  return array[getNextIndex(current, direction, array.length)];
-}
+const getNextElement =
+    (array: any[], current: number, direction: number) => {
+      return array[getNextIndex(current, direction, array.length)];
+    }
 
 const style: any = {
   wrapper: css({
@@ -70,9 +74,7 @@ const style: any = {
     back: css({
       transform: 'rotateY(180deg) translate3d(0, 0, 200px)',
     }),
-    current: css({
-      backgroundColor: '#f5f5f5'
-    })
+    current: css({backgroundColor: '#f5f5f5'})
   },
   shadow: {
     holder: css({
@@ -95,7 +97,7 @@ const style: any = {
       boxShadow: '0px 0px 500px 500px black',
       backgroundColor: 'black'
     })
-    },
+  },
   nav: {
     base: css({
       display: 'flex',
@@ -113,23 +115,9 @@ const style: any = {
         paddingRight: '5px',
       }
     }),
-    item: css({
-      transition: 'color 1s ease',
-      '&:hover': {
-        cursor: 'pointer'
-      }
-    }),
-    link: css({
-      color: '#444',
-      '&:hover': {
-        color: '#aaa'
-      }
-    }),
-    selected: css({
-      'a': {
-        color: '#000'
-      }
-    })
+    item: css({transition: 'color 1s ease', '&:hover': {cursor: 'pointer'}}),
+    link: css({color: '#FFF', '&:hover': {color: '#2E93FF'}}),
+    selected: css({'a': {color: '#2E93FF'}})
   },
   description: {
     holder: css({
@@ -154,7 +142,7 @@ const style: any = {
   }
 }
 
-const orientations = ["front", "right", "back", "left"];
+const orientations = ['front', 'right', 'back', 'left'];
 
 const rotations: any = {
   front: 0,
@@ -166,54 +154,29 @@ const rotations: any = {
 const animations: any = {
   description: {
     left: {
-      in: keyframes({
-        '0%': {
-          opacity: '0',
-          left: '-100%'
-        },
-        '100%': {
-          opacity: '1',
-          left: '0%'
-        }
+      in : keyframes({
+        '0%': {opacity: '0', left: '-100%'},
+        '100%': {opacity: '1', left: '0%'}
       }),
       out: keyframes({
-        '0%': {
-          opacity: '1',
-          left: '0%'
-        },
-        '100%': {
-          opacity: '0',
-          left: '100%'
-        }
+        '0%': {opacity: '1', left: '0%'},
+        '100%': {opacity: '0', left: '100%'}
       }),
     },
     right: {
-      in: keyframes({
-        '0%': {
-          opacity: '0',
-          left: '100%'
-        },
-        '100%': {
-          opacity: '1',
-          left: '0%'
-        }
+      in : keyframes({
+        '0%': {opacity: '0', left: '100%'},
+        '100%': {opacity: '1', left: '0%'}
       }),
       out: keyframes({
-        '0%': {
-          opacity: '1',
-          left: '0%'
-        },
-        '100%': {
-          opacity: '0',
-          left: '-100%'
-        }
+        '0%': {opacity: '1', left: '0%'},
+        '100%': {opacity: '0', left: '-100%'}
       }),
     }
   }
 }
 
 export default class View extends EventEmitter {
-
   private model: Model;
   private elementId: string;
   private element: HTMLElement;
@@ -236,12 +199,13 @@ export default class View extends EventEmitter {
       urls: [],
       descriptions: [],
       cursor: 0,
-      direction: "left"
+      direction: 'left'
     }
 
-    this.elementId = elementId;
+                 this.elementId = elementId;
 
     const element = document.getElementById(elementId);
+
     if (!element) {
       throw new Error(`Element with id ${elementId} does not exists`);
     }
@@ -250,33 +214,42 @@ export default class View extends EventEmitter {
 
     this.init();
 
-    window.addEventListener('resize', () => this.resize(), false)
+    window.addEventListener('resize', () => this.resize(), false);
     this.resize();
   }
 
   private resize() {
-    const cubeElement = this.element.getElementsByClassName(style.cube)[0] as HTMLElement;
-    const { clientWidth, clientHeight } = cubeElement;
+    const cubeElement =
+        this.element.getElementsByClassName(style.cube)[0] as HTMLElement;
+    const {clientWidth, clientHeight} = cubeElement;
     const size = Math.min(clientWidth, clientHeight);
 
-    this.distance = size/2;
+    this.distance = size / 2;
 
     orientations.forEach((orientation: string) => {
-      const faceElement = this.element.getElementsByClassName(style.face[orientation])[0] as HTMLElement;
-      faceElement.style.transform = `rotateY(${rotations[orientation]}deg) translate3d(0, 0, ${this.distance}px)`;
-      faceElement.style.boxShadow = `rgba(0, 0, 0, 0.35) 0px 0px ${size/10}px ${size/20}px inset`;
+      const faceElement = this.element.getElementsByClassName(
+                              style.face[orientation])[0] as HTMLElement;
+      faceElement.style.transform = `rotateY(${
+          rotations[orientation]}deg) translate3d(0, 0, ${this.distance}px)`;
+      faceElement.style.boxShadow =
+          `rgba(0, 0, 0, 0.35) 0px 0px ${size / 10}px ${size / 20}px inset`;
     });
 
-    const shadowHolderElements = cubeElement.getElementsByClassName(style.shadow.holder);
-    Array.from(shadowHolderElements).forEach((shadowHolderElement: HTMLElement) => {
-      shadowHolderElement.style.width = `${size}px`;
-      shadowHolderElement.style.height = `${size}px`;
-      shadowHolderElement.style.transform = `translateY(${this.distance + size/30}px) rotateX(-90deg)`;
-    });
+    const shadowHolderElements =
+        cubeElement.getElementsByClassName(style.shadow.holder);
+    Array.from(shadowHolderElements)
+        .forEach((shadowHolderElement: HTMLElement) => {
+          shadowHolderElement.style.width = `${size}px`;
+          shadowHolderElement.style.height = `${size}px`;
+          shadowHolderElement.style.transform =
+              `translateY(${this.distance + size / 30}px) rotateX(-90deg)`;
+        });
 
-    const shadowElements = cubeElement.getElementsByClassName(style.shadow.item);
+    const shadowElements =
+        cubeElement.getElementsByClassName(style.shadow.item);
     Array.from(shadowElements).forEach((shadowElement: HTMLElement) => {
-      shadowElement.style.boxShadow = `0px 0px ${size/4}px ${size/3}px black`;
+      shadowElement.style.boxShadow =
+          `0px 0px ${size / 4}px ${size / 3}px black`;
     });
 
     this.updateCube();
@@ -286,7 +259,8 @@ export default class View extends EventEmitter {
     this.element.innerHTML = `
     <div class="${style.wrapper}">
     <div class="${style.cube}">
-    <div class="${style.face.common} ${style.face.front} ${style.face.current}"></div>
+    <div class="${style.face.common} ${style.face.front} ${
+        style.face.current}"></div>
     <div class="${style.face.common} ${style.face.left}"></div>
     <div class="${style.face.common} ${style.face.right}"></div>
     <div class="${style.face.common} ${style.face.back}"></div>
@@ -304,7 +278,8 @@ export default class View extends EventEmitter {
   }
 
   public update(model: Model) {
-    const navNeedsRebuild = this.model.urls.toString() !== model.urls.toString();
+    const navNeedsRebuild =
+        this.model.urls.toString() !== model.urls.toString();
     if (navNeedsRebuild) {
       this.rebuildNav(model);
     }
@@ -312,7 +287,8 @@ export default class View extends EventEmitter {
     const needsToMove = model.cursor != this.model.cursor;
     if (needsToMove) {
       this.currentDirection = model.direction == 'right' ? 1 : -1;
-      this.currentFaceId = getNextIndex(this.currentFaceId, this.currentDirection, orientations.length);
+      this.currentFaceId = getNextIndex(
+          this.currentFaceId, this.currentDirection, orientations.length);
       this.move(model);
     }
 
@@ -329,9 +305,9 @@ export default class View extends EventEmitter {
 
   private updateDescription(model: Model) {
     const text = model.descriptions[model.cursor];
-    const descriptions: HTMLElement[] = Array.from(
-      this.element.getElementsByClassName(style.description.item)
-    ) as HTMLElement[];
+    const descriptions: HTMLElement[] =
+        Array.from(this.element.getElementsByClassName(
+            style.description.item)) as HTMLElement[];
 
     if (this.model.cursor == model.cursor) {
       descriptions[0].innerHTML = model.descriptions[model.cursor];
@@ -340,15 +316,17 @@ export default class View extends EventEmitter {
 
     // out element
     descriptions[1].innerHTML = this.model.descriptions[this.model.cursor];
-    descriptions[1].style.animationName = "none";
+    descriptions[1].style.animationName = 'none';
     descriptions[1].offsetHeight;
-    descriptions[1].style.animationName = animations.description[model.direction].out;
+    descriptions[1].style.animationName =
+        animations.description[model.direction].out;
 
     // in element
     descriptions[0].innerHTML = model.descriptions[model.cursor];
-    descriptions[0].style.animationName = "none";
+    descriptions[0].style.animationName = 'none';
     descriptions[0].offsetHeight;
-    descriptions[0].style.animationName = animations.description[model.direction].in;
+    descriptions[0].style.animationName =
+        animations.description[model.direction].in;
   }
 
   private rebuildNav(model: Model) {
@@ -362,19 +340,19 @@ export default class View extends EventEmitter {
       let item = this.navItems[url];
 
       if (!item) {
-        item = document.createElement("li");
-        const itemLink = document.createElement("a");
+        item = document.createElement('li');
+        const itemLink = document.createElement('a');
         itemLink.classList.add(style.nav.link);
         item.appendChild(itemLink);
         itemLink.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box" viewBox="0 0 16 16">
           <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5 8.186 1.113zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
         </svg>
-        `
-        item.classList.add(style.nav.item)
-        itemLink.addEventListener("click", () => {
-          this.emit("selected", id);
-        })
+        `;
+        item.classList.add(style.nav.item);
+        itemLink.addEventListener('click', () => {
+          this.emit('selected', id);
+        });
         this.navItems[url] = item;
       }
 
@@ -393,28 +371,36 @@ export default class View extends EventEmitter {
   }
 
   private updateCube() {
-    const cubeElement = this.element.getElementsByClassName(style.cube)[0] as HTMLElement;
-    cubeElement.style.transform = `translateZ(-${this.distance}px) rotateY(${this.angle}deg)`;
+    const cubeElement =
+        this.element.getElementsByClassName(style.cube)[0] as HTMLElement;
+    cubeElement.style.transform =
+        `translateZ(-${this.distance}px) rotateY(${this.angle}deg)`;
   }
 
   private updateFaces(model: Model) {
-    Array.from(this.element.getElementsByClassName(style.face.common)).forEach((element: HTMLElement) => {
-      element.classList.remove(style.face.current);
-    });
+    Array.from(this.element.getElementsByClassName(style.face.common))
+        .forEach((element: HTMLElement) => {
+          element.classList.remove(style.face.current);
+        });
 
     const orientation = orientations[this.currentFaceId];
-    const face = this.element.getElementsByClassName(style.face[orientation])[0] as HTMLElement;
+    const face = this.element.getElementsByClassName(
+                     style.face[orientation])[0] as HTMLElement;
     face.classList.add(style.face.current);
 
     for (let i = 0; i < 3; i++) {
-      const orientation = getNextElement(orientations, this.currentFaceId, i*this.currentDirection);
-      const face = this.element.getElementsByClassName(style.face[orientation])[0] as HTMLElement;
-      this.updateBackgroundImage(face, getNextElement(model.urls, model.cursor, i*this.currentDirection));
+      const orientation = getNextElement(
+          orientations, this.currentFaceId, i * this.currentDirection);
+      const face = this.element.getElementsByClassName(
+                       style.face[orientation])[0] as HTMLElement;
+      this.updateBackgroundImage(
+          face,
+          getNextElement(model.urls, model.cursor, i * this.currentDirection));
     }
   }
 
   private move(model: Model) {
-    this.angle -= this.currentDirection*90;
+    this.angle -= this.currentDirection * 90;
   }
 
   private updateBackgroundImage(element: HTMLElement, url: string) {
@@ -427,7 +413,8 @@ export default class View extends EventEmitter {
   }
 
   private async getObjectUrl(url: string) {
-    const getObjectUrlResolver = async (url: string) => {
+    const getObjectUrlResolver =
+        async (url: string) => {
       const response = await fetch(url);
       const blob = await response.blob();
       const objectUrl = URL.createObjectURL(blob);
@@ -440,5 +427,4 @@ export default class View extends EventEmitter {
 
     return this.urlResolvers[url];
   }
-
 }
